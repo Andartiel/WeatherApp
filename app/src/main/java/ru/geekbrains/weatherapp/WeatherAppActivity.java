b24b5ad584e2e46a9fd633b1a78b2e30a08deea1
Ivan <andartiel@mail.ru>
2019-04-22 00:45:31 +0100

    Lessson_2_HW

--- app/src/main/java/ru/geekbrains/weatherapp/WeatherAppActivity.java
+++ app/src/main/java/ru/geekbrains/weatherapp/WeatherAppActivity.java
@@ -1,13 +1,105 @@
 package ru.geekbrains.weatherapp;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WeatherAppActivity extends AppCompatActivity {
+
+    private static final String TAG = "WeatherAppActivity";
+
+
+    Date currentDate = new Date();
+    SimpleDateFormat formatForDateNow = new SimpleDateFormat("E dd.MM.yyyy 'время' hh:mm:ss");
+    private TextView dateView;
+    private ImageView sunIcon;
+    private Button refreshButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String instanceState;
+        if (savedInstanceState == null) {
+            instanceState = "Приветствую!";
+        } else {
+            instanceState = "Повторный запуск!";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_weatherapp);
+
+        dateView = (TextView) findViewById(R.id.dateView);
+        dateView.setText(formatForDateNow.format(currentDate));
+
+        sunIcon = (ImageView) findViewById(R.id.sunnyIcon);
+        sunIcon.setImageBitmap(BitmapFactory.decodeResource(this.getResources(), R.drawable.sun));
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        Toast.makeText(getApplicationContext(), "Поворот экрана!! - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+        Log.d(TAG, "onSaveInstanceState() called");
+        Toast.makeText(getApplicationContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
